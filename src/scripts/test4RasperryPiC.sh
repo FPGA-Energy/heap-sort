6dcc5452e016a9e87b6fbebbce4c8c16328f624d
Maja Hanne Kirkeby <MHKirkeby@users.noreply.github.com>
2023-01-24 16:39:55 +0100

    Update test4RasperryPiC.sh
    test script is updated to create a CSV file

--- src/scripts/test4RasperryPiC.sh
+++ src/scripts/test4RasperryPiC.sh
@@ -9,22 +9,22 @@
 
 	benchcmd() {
 
-			echo "$@;"  >> $LOGFILE
+#			echo "$@;"  >> $LOGFILE
 			T="$(date +%s%N)"
 			eval "$@"
 			T="$(($(date +%s%N)-T))"
 			T=$((T/1000000))
-			echo "${T};::" >> $LOGFILE
-			#sleep 1
+			echo "$@;${T};" >> $LOGFILE
+			sleep 5
 
 	}
 
 	run_files() {
+		echo "[$(date)]" >> $LOGFILE		
 
 		for file in $1
 		do 
 			echo $file 
-			echo "[$(date)]" >> $LOGFILE		
 			for j1 in 1 2 3 4 5 6 7 8 9 10
 			do	
 				echo "$j1; "
@@ -33,15 +33,15 @@
 				do	
 					for i in 1 2 3 4
 					do
-						echo "$j1*$j2*$i; " >> $LOGFILE
+						#echo "$j1*$j2*$i; " >> $LOGFILE
 						benchcmd $file 
 
 					done
 				done
 
 			done
-			echo "[$(date)]" >> $LOGFILE
-			#sleep 20
+			#echo "[$(date)]" >> $LOGFILE
+			sleep 10
 
 		done
 
